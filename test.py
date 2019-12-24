7442db46857b043469567949beca1a683924f091
Matheus <mathu.fnn@gmail.com>
2019-12-24 10:12:17 -0300

    busca de todas as matérias e suas justificações concluída

--- test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get('http://legis.senado.leg.br/dadosabertos/materia/pesquisa/lista?ano=2013')
-with open('teste.xml', 'wb') as file:
-  file.write(response.content)
